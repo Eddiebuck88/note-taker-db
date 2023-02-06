f3f8e6a49587343186bc7ac532c1822aeab88fc8
edmond buck <edmond.buck@wearetissue.com>
2023-02-06 00:19:04 -0800

    added to more server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
